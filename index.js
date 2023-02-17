4a7fa58c44f791baa6f65d7a7a184a83a104853f
Darlington Damola <darlingtondamola@gmail.com>
2023-02-17 09:32:43 +0100

    New

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './Appsss';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
